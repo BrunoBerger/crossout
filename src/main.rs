6dd57ff3255954a05057ab78392b464bc06e7c74
BrunoBerger <bruno.berger@stud.hshl.de>
2023-09-23 18:41:16 +0200

    vec structure

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use iced::executor;
-use iced::widget::{checkbox, column, container};
+use iced::widget;
+use iced::widget::row;
+use iced::widget::{checkbox, column, container, button, text};
 use iced::{Application, Command, Element, Length, Settings, Theme};
 
+const OFFSET: usize = 3;
 
 pub fn main() -> iced::Result {
     MyApp::run(Settings::default())
@@ -10,19 +13,21 @@ pub fn main() -> iced::Result {
 #[derive(Default)]
 struct MyApp {
     default_checkbox: bool,
+    counter: u32,
     numbers: Vec<u8>,
     mask: Vec<u8>
 }
 impl MyApp {
     fn new() -> Self {
-        MyApp { default_checkbox: false, numbers: vec![1,2,3,4], mask: vec![0,0,0,0] }
+        MyApp { default_checkbox: false, numbers: vec![1,2,3,4,5,6], mask: vec![0,0,0,0] ,counter: 0}
     }
 }
 
 #[derive(Debug, Clone, Copy)]
 enum Message {
     DefaultChecked(bool),
-    NewGame(),
+    NewGame,
+    NumberPressed,
 }
 
 impl Application for MyApp {
@@ -46,23 +51,50 @@ impl Application for MyApp {
     fn update(&mut self, message: Message) -> Command<Message> {
         match message {
             Message::DefaultChecked(value) => self.default_checkbox = value,
-            Message::NewGame() => {},
+            Message::NewGame => {println!("test")},
+            Message::NumberPressed => {},
         }
 
         Command::none()
     }
 
     fn view(&self) -> Element<Message> {
-        let default_checkbox =
-            checkbox("Default", self.default_checkbox, Message::DefaultChecked);
+        println!("View updated");
+        let default_checkbox = checkbox("Default", self.default_checkbox, Message::DefaultChecked);
+        let new_game_button = button("Press me!").on_press(Message::NewGame);
 
-        for n in &self.numbers {
-            println!("{n}");
-        }
+        let mut number_col = column![];
+
+        let mut irow = widget::Row::new();
+    
         
-        let content = column![default_checkbox].spacing(10);
+        for (i, n) in self.numbers.iter().enumerate() {
+            let new_button = button("n").on_press(Message::NumberPressed);
+            irow = irow.push(new_button);
+            if i % OFFSET <= 0 {
+                number_col = number_col.push(irow);
+                irow = widget::Row::new();
+            }
+        }
+        number_col = number_col.push(irow);
+        
+        // let mut test_row = row![
+        //     button("Pstese").on_press(Message::NewGame),
+        //     button("Presssefef").on_press(Message::NewGame),
+        // ];
 
-        container(content)
+        // let sub_col = widget::column(number_content);
+
+        let collumn = column![
+            default_checkbox,
+            new_game_button,
+            // test_row,
+            number_col,
+            text(self.counter).size(50),
+            ].spacing(10);
+
+
+        container(collumn)
             .width(Length::Fill)
             .height(Length::Fill)
             .center_x()
